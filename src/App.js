ba0e49783d9ec98e1f5c1611efc754bb55ca890e
ramarm <ramperel98@gmail.com>
2024-03-08 20:44:14 +0100

    Added multisteps indicator

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import './App.css';
-import {EditableText} from "monday-ui-react-core";
+import {EditableText, MultiStepIndicator} from "monday-ui-react-core";
 import "monday-ui-react-core/tokens";
 
 
 function App() {
-  return (
-      <EditableText
-          type={EditableText.types.TEXT2}
-          weight={EditableText.weights.BOLD}
-          placeholder="New report"
-          value={''}
-          onChange={(newTitle) => {
-            console.log(newTitle);
-          }}
-      />
-  );
+    const steps = [
+        {
+            key: "first",
+            titleText: "First"
+        },
+        {
+            key: "second",
+            titleText: "Second"
+        },
+        {
+            key: "third",
+            titleText: "Third"
+        }
+    ]
+    return <div>
+        <EditableText
+            type={EditableText.types.TEXT2}
+            weight={EditableText.weights.BOLD}
+            placeholder="New report"
+            value={''}
+            onChange={(newTitle) => {
+                console.log(newTitle);
+            }}/>
+        <MultiStepIndicator size="compact"
+                            steps={steps}/>
+    </div>;
 }
 
 export default App;
