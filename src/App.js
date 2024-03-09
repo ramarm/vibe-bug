92b4f91e33622614e0678ce7a42d2708dd6ba8e8
ramarm <ramperel98@gmail.com>
2024-03-09 16:46:22 +0100

    Added combobox bug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-import {EditableText, MultiStepIndicator} from "monday-ui-react-core";
+import {
+    Modal,
+    Button,
+    Dialog,
+    DialogContentContainer,
+    Combobox,
+    EditableText,
+    MultiStepIndicator
+} from "monday-ui-react-core";
 import "monday-ui-react-core/tokens";
+import {useState} from "react";
 
 
 function App() {
+    const [isOpen, setIsOpen] = useState(false);
     const steps = [
         {
             key: "first",
@@ -29,6 +39,43 @@ function App() {
             }}/>
         <MultiStepIndicator size="compact"
                             steps={steps}/>
+        <Button onClick={() => setIsOpen(true)}>Open modal</Button>
+        <Dialog zIndex={2000000} position={Dialog.positions.BOTTOM}
+                content={<DialogContentContainer>
+                    <Combobox optionClassName="insight-combobox"
+                              size={Combobox.sizes.SMALL}
+                              maxOptionsWithoutScroll={5}
+                              onClick={(value) => console.log(value)}
+                              placeholder="Search"
+                              options={[
+                                  {id: "1", label: "One"},
+                                  {id: "2", label: "Two"},
+                                  {id: "3", label: "Three"}
+                              ]}/>
+                </DialogContentContainer>}
+                showTrigger={[Dialog.hideShowTriggers.CLICK]}
+                hideTrigger={[Dialog.hideShowTriggers.CLICK, Dialog.hideShowTriggers.CLICK_OUTSIDE]}>
+            <Button>Open dialog</Button>
+        </Dialog>
+        <Modal title="test" onClose={() => setIsOpen(false)} show={isOpen}>
+            <Dialog zIndex={2000000} position={Dialog.positions.BOTTOM}
+                    content={<DialogContentContainer>
+                        <Combobox optionClassName="insight-combobox"
+                                  size={Combobox.sizes.SMALL}
+                                  maxOptionsWithoutScroll={5}
+                                  onClick={(value) => console.log(value)}
+                                  placeholder="Search"
+                                  options={[
+                                      {id: "1", label: "One"},
+                                      {id: "2", label: "Two"},
+                                      {id: "3", label: "Three"}
+                                  ]}/>
+                    </DialogContentContainer>}
+                    showTrigger={[Dialog.hideShowTriggers.CLICK]}
+                    hideTrigger={[Dialog.hideShowTriggers.CLICK, Dialog.hideShowTriggers.CLICK_OUTSIDE]}>
+                <Button>Open dialog</Button>
+            </Dialog>
+        </Modal>
     </div>;
 }
 
